70988045c17537275445a0b92109bfa333bea6d0
cc <cc@qq.com>
2023-05-06 15:21:31 +0800

    feat: update

--- src/layouts/basic-layout/index.tsx
+++ src/layouts/basic-layout/index.tsx
@@ -1,8 +1,17 @@
 import { defineComponent } from 'vue'
+import { useAppStore, useThemeStore } from '@/store'
+import { useBasicLayout } from '@/composable'
+
+import AdminLayout from '../common/admin-layout'
+import './index.scss'
 
 export default defineComponent({
 	name: 'BasicLayout',
 	setup() {
-		return () => <div>BasicLayout</div>
+		const app = useAppStore()
+		const theme = useThemeStore()
+
+		const { mode, headerProps, sideVisible, sideWidth, sideCollapsedWidth } = useBasicLayout()
+		return () => <AdminLayout mode={mode.value}></AdminLayout>
 	}
 })
